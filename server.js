1b1465b59ea7978827127e2cf716a29bf94c0d5d
Kelley Worthington <kelleymarne@gmail.com>
2021-07-02 09:44:47 -0600

    validation added

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 
@@ -36,6 +41,35 @@ function findById(id, animalsArray) {
     return result;
 }
 
+
+function createNewAnimal(body, animalsArray) {
+    //functions main code here
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if(!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personailtyTraits || !Array.isArray(animal.personailtyTraits)) {
+        return false;
+    }
+
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -51,6 +85,23 @@ app.get('/api/animals/:id', (req, res) => {
    } else {
        res.send(404);
    }
+});
+
+app.post('/api/animals', (req, res) => {
+    //  sets an id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file an danimals array in this function
+        const animal = createNewAnimal(req.body, animals);
+    
+        res.json(animal);
+
+    }
+
 });
 
 app.listen(PORT, () => {
